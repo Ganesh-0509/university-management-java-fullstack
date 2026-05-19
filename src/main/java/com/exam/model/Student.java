5b0c16db35d7e7844c46edf5ecbbc76a0afd3c4d
Ganesh-0509 <ganesh957kumar@gmail.com>
2026-05-19 16:29:14 +0530

    final

--- src/main/java/com/exam/model/Student.java
+++ src/main/java/com/exam/model/Student.java
@@ -1,8 +1,16 @@
 package com.exam.model;
 
-import jakarta.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "students")
